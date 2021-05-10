47bf2d2fd5d3daa973a361d904fed8e2d76bff08
优雅 <1301884419@qq.com>
2021-05-10 09:09:33 +0800

    zhk

---
+++ src/components/yy_FilterBySubject.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <el-select v-model="value" @change="changeValue" clearable placeholder="请输入学科">
+      <el-option
+              v-for="item in data"
+              :key="item.value"
+              :label="item.label"
+              :value="item.value">
+      </el-option>
+    </el-select>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "yy_FilterBySubject",
+    data() {
+      return {
+        data:[{
+          value:'wuwen',
+          label:'语文'
+        },{
+          value:'yingwu',
+          label:'英语'
+        },{
+          value:'shuxue',
+          label:'数学'
+        }],
+        value: ''
+      }
+    },
+    methods:{
+      changeValue(){
+        console.log(this.value);
+      }
+    },
+  }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/yy_FilterByTestType.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <el-select v-model="value" @change="changeValue" clearable placeholder="请输入考试类型">
+      <el-option
+              v-for="item in data"
+              :key="item.value"
+              :label="item.label"
+              :value="item.value">
+      </el-option>
+    </el-select>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "yy_FilterByTestType",
+    data() {
+      return {
+        data:[{
+          value:'yuekao',
+          label:'月考'
+        },{
+          value:'qizhong',
+          label:'期中'
+        },{
+          value:'qimo',
+          label:'期末'
+        }],
+        value: ''
+      }
+    },
+    methods:{
+      changeValue(){
+        console.log(this.value);
+      }
+    },
+  }
+</script>
+
+<style scoped>
+
+</style>
+

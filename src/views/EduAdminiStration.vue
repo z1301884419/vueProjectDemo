61d9f49339d6dc913290f7ced9993cb64b5e9504
qianwang <727767815@qq.com>
2021-05-10 17:05:01 +0800

    ALAIWA

---
+++ src/views/EduAdminiStration.vue
@@ -0,0 +1,146 @@
+<template>
+  <div class="eduAdm">
+    <!-- 面包屑导航部分 -->
+    <el-breadcrumb separator-class="el-icon-arrow-right">
+      <el-breadcrumb-item :to="{ path: '/Home/HomePage' }">首页</el-breadcrumb-item>
+      <el-breadcrumb-item>师资管理</el-breadcrumb-item>
+      <el-breadcrumb-item>教务管理</el-breadcrumb-item>
+    </el-breadcrumb>
+    <el-divider></el-divider>
+
+    <!-- 搜索框 -->
+    <el-form :inline="true" :model="searchForm" class="demo-form-inline">
+      <el-form-item>
+        <el-button class="successBtn">添加班级</el-button>
+      </el-form-item>
+      <el-form-item>
+        <el-select v-model="searchForm.course" placeholder="请选择年级">
+          <el-option label="高一" value="shanghai"></el-option>
+          <el-option label="高二" value="beijing"></el-option>
+        </el-select>
+      </el-form-item>
+      <el-form-item>
+        <el-select v-model="searchForm.course" placeholder="请选择年级">
+          <el-option label="一班" value="shanghai"></el-option>
+          <el-option label="二班" value="beijing"></el-option>
+        </el-select>
+      </el-form-item>
+      <el-form-item>
+        <el-button plain class="plainBtn">查询</el-button>
+        <el-button type="info" plain>重置</el-button>
+      </el-form-item>
+    </el-form>
+
+    <!-- 班级卡片 -->
+    <div class="cardBox">
+      <el-col :span="4" v-for="item in tableData" :key="item.id">
+        <el-card shadow="hover">
+          <p>{{ item.className }}</p>
+          <p>班主任: {{item.headTeacher==""?"未分配":item.headTeacher}}</p>
+          <div v-if="item.headTeacher===''"><el-button round plain class="plainBtn">分配班主任</el-button></div>
+          <div v-else><el-button round plain class="plainBtn">调换班主任</el-button></div>
+          <div><el-button round plain class="plainBtn">分配任课教师</el-button></div>
+          <div><el-button round plain class="plainBtn">删除班级</el-button></div>
+        </el-card>
+      </el-col>
+    </div>
+    <!-- 分页 -->
+    <el-pagination layout="prev, pager, next" :total="totalLength" :page-size="pageSize" @current-change="handleCurrentChange"></el-pagination>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      tableData: [
+        { id: 1, className: "高一一班" , headTeacher: ""},
+        { id: 2, className: "高一二班" , headTeacher: "李四"},
+        { id: 3, className: "高一三班" , headTeacher: "张三"},
+        { id: 4, className: "高一四班" , headTeacher: "王五"},
+        { id: 5, className: "高一五班" , headTeacher: ""},
+        { id: 6, className: "高一六班" , headTeacher: ""},
+        { id: 7, className: "高一七班" , headTeacher: ""},
+      ],
+      searchForm: {},
+      pageSize: 12, //一页显示多少条
+      totalLength: 20, //一共有多少条数据
+    };
+  },
+  methods: {
+    handleCurrentChange(val) { // 获取页码
+      console.log(`当前页: ${val}`);
+    },
+  },
+};
+</script>
+
+<style lang="less" scoped>
+/* 78c5b0  C5E3D2   B2DBD4  16B387  F9D789  D4EDF9 EBF3FB  87C1EB FDF17F 8ADCFF F66077*/
+/* 面包屑导航 */
+.el-breadcrumb {
+  line-height: 3;
+  padding-left: 1rem;
+  font-size: 1.6rem;
+}
+.el-divider {
+  margin: 0;
+}
+/* 搜索框按钮样式 */
+.successBtn {
+  background-color: #16B387;
+  border: 1px solid #16B387;
+  color: #fff;
+}
+.successBtn:hover {
+  border: 1px solid #16B387;
+  color: #16B387;
+  background-color: white;
+}
+.plainBtn {
+  border: 1px solid #b2dbd4;
+  color: #16b387;
+}
+.plainBtn:hover {
+  background-color: #b2dbd4;
+  border: 1px solid #b2dbd4;
+  color: #fff;
+}
+// 搜索框
+.el-form{
+  padding-left: 1.5%;
+  margin-top: 1rem;
+  .successBtn{
+    margin-right: 15rem;
+  }
+  .el-form-item{
+    margin: 1rem;
+  }
+}
+// 卡片
+.cardBox{
+  padding: 2rem;
+  overflow: hidden;
+}
+.el-card {
+  background-color: #fdf07f85;
+  text-align: center;
+  border-radius: 2rem;
+  min-height: 25rem;
+  margin: 1rem;
+  .el-button {
+    width: 90%;
+    margin: .5rem 1rem;
+  }
+  p{
+    font-size: 1.6rem;
+  }
+  p:nth-child(1){
+    color: #16b387;
+    font-weight: 600;
+  }
+}
+// 分页
+.el-pagination {
+  text-align: center;
+}
+</style>
---
+++ src/views/TeacherInfo.vue
@@ -0,0 +1,272 @@
+<template>
+  <div class="teInfo">
+    <!-- 面包屑导航部分 -->
+    <el-breadcrumb separator-class="el-icon-arrow-right">
+      <el-breadcrumb-item :to="{ path: '/Home/HomePage' }">首页</el-breadcrumb-item>
+      <el-breadcrumb-item>师资管理</el-breadcrumb-item>
+      <el-breadcrumb-item>教师信息管理</el-breadcrumb-item>
+    </el-breadcrumb>
+    <el-divider></el-divider>
+
+    <!-- 搜索部分 -->
+    <el-form :inline="true" :model="searchForm" class="demo-form-inline">
+      <el-form-item>
+        <el-button class="successBtn" @click="addTeacher = true">添加教师</el-button>
+      </el-form-item>
+      <el-form-item>
+        <el-select v-model="searchForm.course" placeholder="请选择任职课程">
+          <el-option label="语文" value="shanghai"></el-option>
+          <el-option label="数学" value="beijing"></el-option>
+        </el-select>
+      </el-form-item>
+      <el-form-item>
+        <el-input v-model="searchForm.code" placeholder="请输入教师编号"></el-input>
+      </el-form-item>
+      <el-form-item>
+        <el-button plain class="plainBtn">查询</el-button>
+        <el-button type="info" plain>重置</el-button>
+      </el-form-item>
+    </el-form>
+
+    <!-- 表格部分 -->
+    <el-table :data="tableData" style="width: 96%" >
+      <el-table-column prop="staff_number" label="教师编号" align="center"></el-table-column>
+      <el-table-column prop="staff_name" label="姓名" align="center"></el-table-column>
+      <el-table-column prop="subject_id" label="课程代号" align="center"></el-table-column>
+      <el-table-column prop="subject_name" label="课程名称" align="center"></el-table-column>
+      <el-table-column prop="staff_entry_time" label="入职日期" align="center"></el-table-column>
+      <el-table-column prop="staff_phone" label="电话" align="center"></el-table-column>
+      <el-table-column label="操作" align="center">
+        <template slot-scope="scope">
+          <el-button class="editBtn" icon="el-icon-edit" circle  @click="modifyDialogShow(scope.row)"></el-button>
+          <el-button class="deleteBtn" type="danger" icon="el-icon-delete" circle></el-button>
+          <el-button class="searchBtn" type="info" icon="el-icon-search" circle></el-button>
+        </template>
+        
+      </el-table-column>
+    </el-table>
+    <!-- 页码 -->
+    <el-pagination layout="prev, pager, next" :total="totalLength" :page-size="pageSize" @current-change="handleCurrentChange"></el-pagination>
+
+    <!-- 模态框 -->
+    <!-- 添加老师的模态框 -->
+    <el-dialog title="添加教师" :visible.sync="addTeacher" width="33%">
+      <el-form :model="addTeachForm" :rules="addTeachrules" ref="addTeachForm" label-width="80px" class="demo-ruleForm">
+        <el-form-item label="工号" prop="staff_number">
+          <el-input v-model="addTeachForm.staff_number"></el-input>
+        </el-form-item>
+        <el-form-item label="姓名" prop="staff_name">
+          <el-input v-model="addTeachForm.staff_name"></el-input>
+        </el-form-item>
+        <el-form-item label="密码" prop="staff_password">
+          <el-input type="password" v-model="addTeachForm.staff_password" autocomplete="off"></el-input>
+        </el-form-item>
+        <el-form-item label="电话" prop="staff_phone">
+          <el-input v-model="addTeachForm.staff_phone"></el-input>
+        </el-form-item>
+        <el-form-item label="年龄" prop="staff_age">
+          <el-input v-model.number="addTeachForm.staff_age"></el-input>
+        </el-form-item>
+        <el-form-item class="btnBox">
+          <el-button @click="addTeacher = false">取 消</el-button>
+          <el-button class="successBtn" @click="addTeacherRequest('addTeachForm')">确 定</el-button>
+        </el-form-item>
+      </el-form>
+    </el-dialog>
+
+    <!-- 删除老师的模态框 -->
+    <!-- <el-dialog title="删除教师" :visible.sync="delTeacher" width="35%">
+      
+      <span slot="footer" class="dialog-footer">
+        <el-button @click="delTeacher = false">取 消</el-button>
+        <el-button class="successBtn" @click="delTeacherRequest">确 定</el-button>
+      </span>
+    </el-dialog> -->
+  </div>
+</template>
+
+<script>
+import TeachInfoMixins from "../mixins/TeachInfoMixins"
+export default {
+  data() {
+    // 手机号验证
+    const validatorPhone = function (rule, value, callback) {
+      if (value === '') {
+        callback(new Error('手机号不能为空'))
+      } else if (!/^1\d{10}$/.test(value)) {
+        callback(new Error('手机号格式错误'))
+      } else {
+        callback()
+      }
+    }
+    return {
+      tableData: [  //总数据
+        {staff_number:"5718349", subject_id: 1, subject_name: "语文", staff_name: "李四", staff_entry_time: "2020-01-29", staff_phone: "15342536353"},
+        {staff_number:"5718349", subject_id: 1, subject_name: "语文", staff_name: "李四", staff_entry_time: "2020-01-29", staff_phone: "15342536353"},
+        {staff_number:"5718349", subject_id: 1, subject_name: "语文", staff_name: "李四", staff_entry_time: "2020-01-29", staff_phone: "15342536353"},
+      ], 
+      pageSize: 5, //一页显示多少条
+      totalLength: 20, //一共有多少条数据
+      searchForm: {},
+      addTeacher: false, //控制添加老师的弹框显示隐藏
+      addTeachForm: {
+        staff_number: '',
+        staff_name: '',
+        staff_password: '',
+        staff_phone: '',
+        staff_age: '',
+      },
+      addTeachrules: {
+        staff_number: [
+          { required: true, message: '工号不能为空'},
+        ],
+        staff_name: [
+          { required: true, message: '请输入教师姓名', trigger: 'blur' },
+          { min: 2, max: 10, message: '长度在 3 到 5 个字符', trigger: 'blur' }
+        ],
+        staff_password: [
+          { required: true, message: '密码不能为空', trigger: 'blur' },
+          { min: 6, max: 20, message: '密码长度应为 6 到 20 个字符', trigger: 'blur' }
+        ],
+        staff_phone: [
+          { required: true, validator: validatorPhone, trigger: 'blur' }
+        ],
+        staff_age: [
+          { required: true, message: '年龄不能为空'},
+          { type: 'number', message: '年龄必须为数字值'}
+        ]
+      },
+      delTeacher: false, //控制删除老师的弹框显示隐藏
+    };
+  },
+  mixins: [TeachInfoMixins],
+  methods: {
+    handleCurrentChange(val) { // 获取页码
+      console.log(`当前页: ${val}`);
+    },
+    addTeacherRequest(formName){ //添加老师的请求
+      this.addTeacher = false;
+      this.$refs[formName].validate((valid) => {
+          if (valid) {
+            // 这里面写请求
+            this.openSuccess("成功", "该教师已成功添加!")
+          } else {
+            console.log('error submit!!');
+            this.$refs[formName].resetFields();
+            this.openError("错误", "您的消息填写有误，未能提交!")
+            return false;
+          }
+        });
+    },
+    modifyDialogShow(row){
+      console.log(row);
+    }
+    
+  },
+};
+</script>
+
+<style lang="less" scoped>
+/* 面包屑导航 */
+.el-breadcrumb {
+  line-height: 3;
+  padding-left: 1rem;
+  font-size: 1.6rem;
+}
+.el-divider {
+  margin: 0;
+}
+/* 搜索框按钮样式 */
+.successBtn {
+  background-color: #16B387;
+  border: 1px solid #16B387;
+  color: #fff;
+}
+.successBtn:hover {
+  border: 1px solid #16B387;
+  color: #16B387;
+  background-color: white;
+}
+.plainBtn {
+  border: 1px solid #16B387;
+  color: #16B387;
+}
+.plainBtn:hover {
+  background-color: #B2DBD4;
+  border: 1px solid #B2DBD4;
+  color: #fff;
+}
+// 搜索框
+.el-form{
+  padding-left: 1.5%;
+  margin-top: 1rem;
+  .successBtn{
+    margin-right: 15rem;
+  }
+  .el-form-item{
+    margin: 1rem;
+  }
+}
+
+/* 表格 */
+.el-table {
+  margin: 2rem auto;
+}
+// 分页
+.el-pagination {
+  text-align: center;
+}
+
+// 模态框
+/deep/.el-dialog{
+  border-radius: 1rem;
+}
+.demo-ruleForm{
+  padding-left: 0;
+  /deep/.el-form-item__content{
+    margin: 1rem;
+  }
+  /deep/.el-form-item__label{
+    margin-top: 10px;
+  }
+  .el-form-item{
+    text-align: left;
+  }
+  .btnBox{
+    margin-top: 3rem;
+    text-align: right;
+    .successBtn{
+      margin-right: 0;
+    }
+  }
+}
+
+// 表格里的按钮样式
+/* 78c5b0  C5E3D2   B2DBD4  16B387  F9D789  D4EDF9 EBF3FB  87C1EB FDF17F 8ADCFF F66077*/
+.editBtn {
+  border: 1px solid #87C1EB;
+  background-color: #87C1EB;
+  color: #fff;
+}
+.editBtn:hover {
+  border: 1px solid #a3cfee;
+  background-color: #a3cfee;
+}
+.deleteBtn {
+  background-color: #F9D789;
+  border: 1px solid #F9D789;
+  color: #08ac7e;
+}
+.deleteBtn:hover {
+  background-color: #f7db9a;
+  border: 1px solid #f7db9a;
+}
+.searchBtn {
+  background-color: #F66077;
+  border: 1px solid #F66077;
+}
+.searchBtn:hover {
+  background-color: #f0798b;
+  border: 1px solid #f0798b;
+}
+</style>

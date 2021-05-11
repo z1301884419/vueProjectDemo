f73464d89429a3eddce656a55135b9b3cb7750cf
qianwang <727767815@qq.com>
2021-05-11 10:36:48 +0800

    123

--- src/views/MemoMgt.vue
+++ src/views/MemoMgt.vue
@@ -10,10 +10,12 @@
         <p class="content">
           <span class="contentTitle">{{ item.teacher }}({{ item.subject }}老师):</span>
           <span>{{ item.content }}</span>
+          <el-input v-if="item.id==id" class="inputBox" type="textarea" v-model="input2"></el-input>
+          <el-button v-if="item.id==id" size="mini" class="successBtn">回复</el-button>
         </p>
         <div>
           <el-button size="mini" round>删除</el-button>
-          <el-button size="mini" round>回复</el-button>
+          <el-button size="mini" v-if="item.id!=id" round @click="replyClick(item.id)">回复</el-button>
         </div>
       </div>
     </el-card>
@@ -24,12 +26,19 @@ export default {
   data(){
     return {
       input: "",
+      input2: "",
+      id: 0,
       tableData: [
         {id:1, teacher:"李四", subject:"语文", content:"学习认真, 作业完成程度好, 继续发扬!!学习认真, 作业完成程度好, 继续发扬!!学习认真, 作业完成程度好, 继续发扬!!学习认真, 作业完成程度好, 继续发扬!!学习认真, 作业完成程度好, 继续发扬!!学习认真, 作业完成程度好, 继续发扬!!学习认真, 作业完成程度好, 继续发扬!!"},
         {id:2, teacher:"张三", subject:"数学", content:"学习认真, 作业完成程度好, 继续发扬!!"},
         {id:3, teacher:"王五", subject:"英语", content:"学习认真, 作业完成程度好, 继续发扬!!学习认真, 作业完成程度好, 继续发扬!!学习认真, 作业完成程度好, 继续发扬!!学习认真, 作业完成程度好, 继续发扬!!学习认真, 作业完成程度好, 继续发扬!!学习认真, 作业完成程度好, 继续发扬!!学习认真, 作业完成程度好, 继续发扬!!"},
         {id:4, teacher:"吴六", subject:"历史", content:"学习认真, 作业完成程度好, 继续发扬!!学习认真, 作业完成程度好, 继续发扬!!学习认真, 作业完成程度好, 继续发扬!!学习认真, 作业完成程度好, 继续发扬!!学习认真, 作业完成程度好, 继续发扬!!"}
       ]
+    }
+  },
+  methods: {
+    replyClick(id){
+      this.id = id;
     }
   }
 }
@@ -58,6 +67,9 @@ export default {
     width: 90%;
     line-height: 2rem;
     padding: 1rem;
+    .inputBox{
+      margin-top: 1rem;
+    }
     .contentTitle{
       color: #16B387;
     }

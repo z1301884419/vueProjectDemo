21d945138e9dbf69941cb02ba069f178cc15f58f
lingling <1554631500@qq.com>
2021-05-17 08:45:33 +0800

    3333

--- vue.config.js
+++ vue.config.js
@@ -1,17 +1,17 @@
-module.exports = {
-    runtimeCompiler: true,
-    publicPath: '/', // 设置打包文件相对路径
-    devServer: {
-        port: 8080,
-        proxy: {
-            '/api': {
-                target: 'http://172.16.14.38:8097/',
-                changeOrigin: true,
-                ws: true,
-                pathRewrite: {
-                    '^/api': ''
-                }
-            }
-        }
-    }
-}
+// module.exports = {
+//     runtimeCompiler: true,
+//     publicPath: '/', // 设置打包文件相对路径
+//     devServer: {
+//         port: 8080,
+//         proxy: {
+//             '/api': {
+//                 target: 'http://172.16.14.38:8097/',
+//                 changeOrigin: true,
+//                 ws: true,
+//                 pathRewrite: {
+//                     '^/api': ''
+//                 }
+//             }
+//         }
+//     }
+// }
